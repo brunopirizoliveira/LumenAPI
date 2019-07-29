3ca9bb0b8f93b53c462681afcaf157550727018f
brunooliverdwl <bruno.pires@qualitor.com.br>
2019-07-29 13:22:36 -0300

    Paginação com paginate

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -7,9 +7,21 @@ abstract class BaseController
 {
     protected $classe;
 
-    public function index()
+    public function index(Request $request)
     {
-        return $this->classe::all();
+        // Paginate do Eloquent
+        return $this->classe::paginate($request->per_page);
+
+        // Paginação manual
+        //$offset = ($request->page - 1) * $request->per_page;
+//        return $this->classe::query()
+//            ->offset($offset)
+//            ->limit($request->per_page)
+//            ->get();
+
+        // Busca todos
+//        return $this->classe::all();
+
     }
 
     public function store(Request $request)
